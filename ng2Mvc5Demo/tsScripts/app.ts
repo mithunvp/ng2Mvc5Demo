9b030b0ea934dfaaf4bb3233ff81b1ed4884d21c
Mithun <mithun.pattankar@gmail.com>
2017-05-26 15:51:21 +0530

    Updated to Angular 4.1 version, removed typings.json

--- ng2Mvc5Demo/tsScripts/app.ts
+++ ng2Mvc5Demo/tsScripts/app.ts
@@ -14,5 +14,5 @@
 export class AppComponent {
     title = 'ASP.NET MVC 5 with Angular 2';
     skills = ['MVC 5', 'Angular 2', 'TypeScript', 'Visual Studio 2015'];
-    myskills = this.skills[1];
+    myskills = this.skills[2];
 }
--- ng2Mvc5Demo/tsScripts/boot.ts
+++ ng2Mvc5Demo/tsScripts/boot.ts
@@ -1,5 +1,4 @@
-﻿///<reference path="./../typings/globals/core-js/index.d.ts"/>
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app';
 
